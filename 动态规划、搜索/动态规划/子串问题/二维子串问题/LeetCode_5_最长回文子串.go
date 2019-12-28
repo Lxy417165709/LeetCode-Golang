5a37b4a95bf27614ca0ca34ae66be59e51fc6274
Lxy417165709 <417165709@qq.com>
2019-12-28 15:01:17 +0800

    结构调整

--- 动态规划、搜索/动态规划/子串问题/二维子串问题/LeetCode_5_最长回文子串.go
+++ 动态规划、搜索/动态规划/子串问题/二维子串问题/LeetCode_5_最长回文子串.go
@@ -3,10 +3,6 @@ package 二维子串问题
 
 // 动态规划 解决最长回文子串
 // dp[i][t] 表示: s[i][t]是否为回文串
-// 状态转移方程:
-//		定义length为子串长度，则:
-// 			length <= 2: dp[begin][end] = s[begin]==s[end]
-//			length >  2: dp[begin][end] = s[begin]==s[end] && dp[begin+1][end-1]
 func longestPalindrome(s string) string {
 	dp := [1050][1050]bool{} // s的最大长度是1000,
 	longestPalindromeString := ""
