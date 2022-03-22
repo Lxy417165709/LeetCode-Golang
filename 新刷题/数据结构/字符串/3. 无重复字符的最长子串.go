c9e2d3de80c44256abb631ba0ed459873ec1291a
xueyueli <xueyueli@tencent.com>
2022-03-22 19:43:45 +0800

    .

--- 新刷题/数据结构/字符串/3. 无重复字符的最长子串.go
+++ 新刷题/数据结构/字符串/3. 无重复字符的最长子串.go
@@ -1,35 +1,30 @@
 package 字符串
 
-import "fmt"
-
-// todo: 这个滑动窗口的定义要确定好！
 func lengthOfLongestSubstring(s string) int {
 	// 1. 空返回。
 	if len(s) == 0 {
 		return 0
 	}
 
-	// 2. 初始化窗口。
-	first, last := 0, 0
+	// 2. 初始化。
+	first, last := 0, 0 // 滑动窗口定义: [first, last) 窗口内的元素不重复。
+	maxLength := 0
 	isInWindow := make(map[byte]bool)
 
 	// 3. 滑动。
-	maxLength := 0
 	for last < len(s) {
-		// 3.1 左指针向右滑动，直到窗口内无重复元素。
-		for first <= last && isInWindow[s[last]] {	// 为什么是 s[last]，而不是s[first]呢？
+		// 3.1 左指针向右滑动，确保窗口内无新元素。 (如果窗口中有新元素，那加入新元素后，窗口就有2个新元素，与定义不符)
+		for first <= last && isInWindow[s[last]] {
 			isInWindow[s[first]] = false
 			first++
 		}
 
-		// 3.2 右指针向右滑动，加入不重复的元素
+		// 3.2 右指针向右滑动，加入新元素。
 		isInWindow[s[last]] = true
+		last++
 
 		// 3.3 获取最长长度。
-		maxLength = max(maxLength, last-first+1)
-		fmt.Println(s[first:last])
-
-		last++
+		maxLength = max(maxLength, last-first)
 	}
 
 	// 4. 返回。
